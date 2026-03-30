afcf2542c5f6b9168faad739bb56c6207287ce0c
unknown <halderurmi@gmail.com>
2026-03-30 14:43:06 +0600

    banner section has completed

--- src/Components/Ui/Banner.jsx
+++ src/Components/Ui/Banner.jsx
@@ -4,7 +4,51 @@ import bannerImage from '../../assets/banner.png';
 
 const Banner = () => {
     return (
-        <div></div>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 py-12 md:py-16 lg:py-20 flex flex-col lg:flex-row items-center gap-10 lg:gap-16">
+            <div className="flex-1 space-y-6 md:space-y-8 text-center lg:text-left">
+                <div className="inline-flex items-center gap-2 bg-[#e1e7ff] text-sm font-medium px-5 py-2 rounded-full mx-auto lg:mx-0">
+                    <CircleDot 
+                        size={18} 
+                        className="text-[#7C3AED]"   
+                    />
+                    <span className='bg-linear-to-r from-[#4F39F6] to-[#9514FA] 
+                        text-transparent bg-clip-text'>New: AI-Powered Tools Available</span>
+                </div>
+                <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold leading-tight tracking-tight text-gray-900">
+                    Supercharge Your <br />
+                    Digital Workflow
+                </h1>
+                <p className="text-xl text-gray-600 max-w-lg">
+                    Access premium AI tools, design assets, templates, and productivity 
+                    software—all in one place. Start creating faster today.<br></br> Explore Products :
+                </p>
+                <div className="flex flex-col sm:flex-row items-center gap-4 pt-4 justify-center lg:justify-start">
+                    <button className="w-full sm:w-auto px-8 py-4 bg-linear-to-r from-[#4F39F6] to-[#9514FA] text-white font-semibold rounded-full hover:brightness-150 transition-all">
+                        Explore Products
+                    </button>
+                    <button className="group flex w-full sm:w-auto items-center justify-center gap-3 px-8 py-4 border-2 border-[#861dfa] hover:-translate-y-2 rounded-full font-semibold transition-all">
+                        <Play 
+                            size={22} 
+                            strokeWidth={3} 
+                            className="text-[#4F39F6] transition-all duration-300 group-hover:scale-110 group-hover:rotate-20" 
+                        />
+                        <span className='bg-linear-to-r from-[#4F39F6] to-[#9514FA] 
+                        text-transparent bg-clip-text transition-all duration-300'>Watch Demo</span>
+                    </button>
+                </div>
+            </div>
+
+            <div className="flex-1 hidden lg:block">
+                <div className="relative rounded-3xl overflow-hidden">
+                    <img 
+                        src={bannerImage} 
+                        alt="Supercharge Your Digital Workflow" 
+                        className="w-full h-auto object-cover"
+                    />
+                </div>
+            </div>
+            
+        </div>
     );
 };
 
